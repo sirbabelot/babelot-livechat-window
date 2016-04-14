900ab39810b81afa151b71726ac23237fed90be7
Dylan Harness <dharness@uwo.ca>
2016-04-14 00:02:38 -0400

    bundled everything

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+var chindowHtml = require('raw!html-minify!./template.html');
+var messenger = require('./Messenger.js');
+var styles = require('css?minimize!./styles.css');
+
+console.log(messenger);
+
+var div = document.createElement('div');
+div.innerHTML = chindowHtml;
+document.body.appendChild(div);
+
+var styleTag = document.createElement('style');
+styleTag.innerHTML = styles;
+document.body.appendChild(styleTag);
+
+messenger.init();
+require('./eventHandler.js');
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build',
+    filename: "bundle.js"
+  }
+};
